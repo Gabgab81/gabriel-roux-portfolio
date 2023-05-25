313bc1dd44462a25dfc96889d2b8f6cd0dbcadf7
gabriel roux <rouxgab81@gmail.com>
2023-05-25 15:02:47 -0400

    all mesh in the scene

---
+++ src/components/canvas/Book.jsx
@@ -0,0 +1,62 @@
+import * as THREE from 'three';
+import React, { useRef, useState, useEffect } from 'react';
+import { useGLTF, Decal, useTexture, RenderTexture } from '@react-three/drei';
+// import { FlakesTexture } from 'three-stdlib';
+
+const Book = () => {
+  // const book = useGLTF("./book/scene.gltf");
+  const { nodes } = useGLTF("./book/scene.gltf");
+  const textures = useTexture("GPT50ForDummies.png")
+
+  return (
+    // <mesh>
+    //   <primitive
+    //     object={book.scene}
+    //     // scale={isMobile ? 0.7 : 0.75}
+    //     position={[0, 2, 0]}
+    //     rotation={[20, 0, 0]}
+    //   />
+      
+    // </mesh>
+    <group>
+      <mesh 
+        castShadow 
+        receiveShadow 
+        geometry={nodes.Architexture_0.geometry}
+        scale={0.3}
+        // position={[0, 0, 0]}
+      >
+        <Decal
+          debug
+          position={[0.4, 0, 0.1]}
+          rotation={[0, 0, 0]}
+          scale={[0.75, 1, 1]}
+          map={textures}
+        >
+          {/* <meshStandardMaterial 
+            metalness={0.2} 
+            roughness={0.8}   
+            // color={"Yellow"} 
+            // map={textures}
+          /> */}
+        </Decal>
+        
+      </mesh>
+      
+      <mesh 
+        castShadow 
+        receiveShadow 
+        geometry={nodes.Architexture_1.geometry}
+      >
+        <meshStandardMaterial 
+          metalness={0.2} 
+          roughness={0.8}   
+          color={"white"} 
+        />
+      </mesh>
+    </group>
+    
+  )
+}
+
+export default Book;
--- src/components/canvas/SceneCanvas.jsx
+++ src/components/canvas/SceneCanvas.jsx
@@ -1,12 +1,41 @@
 import { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import { OrbitControls, Preload } from "@react-three/drei";
 
 import CanvasLoader from '../Loader'
 
+import { Robot } from "./RobotWith3Anim";
+import Room  from "./Room";
+import Book from "./Book";
+import { Dev } from "./DevLaying";
+
 const SceneCanvas = () => {
+
   return (
-    <div>Scene</div>
+    <Canvas
+      // frameloop="demand"
+      shadows
+      camera={ { position: [10, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls 
+          // enableZoom={false} 
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <ambientLight intensity={1} />
+        <group>
+          <Robot />
+          <Room />
+          <Dev />
+          <Book />
+        </group>
+        
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
   )
 }
 
