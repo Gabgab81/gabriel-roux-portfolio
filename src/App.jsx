6486a43234d930c2fea977651986f82e14f24ff2
gabriel roux <rouxgab81@gmail.com>
2023-06-11 16:09:37 -0400

    focus on work

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
           <Navbar />
           {/* <Hero /> */}
           {/* <About /> */}
-          {/* <Works /> */}
+          <Works />
           <Contact />
           {/* <Footer /> */}
         </div> 
