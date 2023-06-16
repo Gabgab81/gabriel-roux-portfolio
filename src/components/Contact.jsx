9ad427c1ce5bf9d432342162c1added1ad879256
gabriel roux <rouxgab81@gmail.com>
2023-06-16 16:48:32 -0400

    suspence done

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,6 @@
 import { Html, Image, OrbitControls, Point, PointMaterial, Points, Sphere, Text, Trail, useHelper } from '@react-three/drei'
 import { Canvas, useFrame } from '@react-three/fiber'
-import React, { useEffect, useRef, useState } from 'react'
+import React, { Suspense, useEffect, useRef, useState } from 'react'
 import { folder, useControls } from 'leva'
 import fontUrl from "./../../public/fonts/ElevateSans.otf"
 import { useContactFrom } from "../contexts/ContactForm"
@@ -12,6 +12,7 @@ import { Billboard } from './canvas/Billboard'
 
 import { Bloom, EffectComposer, Noise, Vignette } from '@react-three/postprocessing'
 import { SectionWrapper } from '../hoc'
+import CanvasLoader from './Loader'
 
 const Contact = () => {
 
@@ -63,19 +64,25 @@ const Contact = () => {
       {/* {!fetchError && !isLoading &&  */}
       <>
         <Canvas camera={{ position: [0, 0, -3], fov:20} } shadows gl={{ antialias: false }} dpr={[1, 1.5]}>
-          <CameraZoom z500={0.60} z750={0.4} z1000={0.5} z1500={0.7} />
-          {/* <OrbitControls /> */}
-          <Moon scale={1.2} position={[-1.1, 0.33, 2.35]} rotation={[0.3, 0.4, 0.5]} />
-          <SpotLight distance={3.8} intensity={3} angle={0.3} position={[0.97, 0.69, 1.12]} rotation={[-1.6, -0.3, -1]} />
-          <Billboard scale={0.1} rotation={[-0.1, -2.7, -0.1]} position={[0.7, -1.4, 0.66]} />
-          <color attach="background" args={['#000']} />
-          <ambientLight intensity={0.01} />
-          <Stars position={[0.18, 0.4, 1.16]} scale={[1.8, 1.8, 0.1]}/>
-          {/* <Stars scale={[1.2, 1.2, 0.1]}/> */}
-          <EffectComposer>
-            <Noise opacity={0.1} />
-            <Vignette eskil={false} offset={0.1} darkness={1.1} />
-          </EffectComposer>
+          
+          <Suspense fallback={<CanvasLoader />}>
+
+            <CameraZoom z500={0.60} z750={0.4} z1000={0.5} z1500={0.7} />
+            {/* <OrbitControls /> */}
+            <Moon scale={1.2} position={[-1.1, 0.33, 2.35]} rotation={[0.3, 0.4, 0.5]} />
+            <SpotLight distance={3.8} intensity={3} angle={0.3} position={[0.97, 0.69, 1.12]} rotation={[-1.6, -0.3, -1]} />
+            <Billboard scale={0.1} rotation={[-0.1, -2.7, -0.1]} position={[0.7, -1.4, 0.66]} />
+            <color attach="background" args={['#000']} />
+            <ambientLight intensity={0.01} />
+            <Stars position={[0.18, 0.4, 1.16]} scale={[1.8, 1.8, 0.1]}/>
+            {/* <Stars scale={[1.2, 1.2, 0.1]}/> */}
+            <EffectComposer>
+              <Noise opacity={0.1} />
+              <Vignette eskil={false} offset={0.1} darkness={1.1} />
+            </EffectComposer>
+
+          </Suspense>
+          
         </Canvas>
       </>
       
--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -5,23 +5,40 @@ const Loader = () => {
 
   return (
     <Html>
-      <span className="canvas-load"></span>
-      <p
-        style={{
-          fontSize: 24,
-          color: "#f1f1f1",
-          fontWeight: 800,
-          marginTop: 40
-        }}
-      >{progress.toFixed(2)}%</p>
-      <p
-        style={{
-          fontSize: 14,
-          color: "#f1f1f1",
-          fontWeight: 800,
-          marginTop: 40
-        }}
-      >{item}</p>
+      {/* <span className="canvas-load"></span> */}
+      <div
+        className="absolute top-[-150px] left-[-250px] w-[500px] h-[200px]
+        flex flex-col justify-center items-center gap-10 "
+      >
+        <div
+          className="bg-[black] rounded-full border-4 border-white h-1/6 w-9/12"
+        >
+          <div
+            className={`bg-[white] rounded-full h-full`}
+            style={{
+              // width: '50%'
+              width: `${progress.toFixed(2)}%`
+            }}
+          >
+          
+          </div>
+        </div>
+        <p
+          className="text-white text-[20px]"
+        >
+          {progress.toFixed(2)}%
+        </p>
+        <p
+          style={{
+            fontSize: 14,
+            color: "#f1f1f1",
+            fontWeight: 800,
+            // marginTop: 40
+          }}
+        >{item}</p>
+
+      </div>
+      
     </Html>
   )
 }
